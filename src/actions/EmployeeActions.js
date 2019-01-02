1a4147bef364467b89122605a75021ef4c4caf71
Michael S <mihai.sanfran@gmail.com>
2019-01-02 15:26:31 -0700

    Create Employee actions file

---
+++ src/actions/EmployeeActions.js
@@ -0,0 +1,11 @@
+import {
+    EMPLOYEE_UPDATE
+} from './types';
+
+// One action creator that can update any different prop that exists inside the form
+export const employeeUpdate = ({ prop, value }) => {
+    return {
+        type: EMPLOYEE_UPDATE,
+        payload: { prop, value }
+    }
+}
