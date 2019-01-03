04f1fdd93205ae0c2961a0901d538cba830763ec
Michael S <mihai.sanfran@gmail.com>
2019-01-03 13:21:17 -0700

    Wire up EmployeeFormReducer to application state as employeeForm

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
+import EmployeeFormReducer from './EmployeeFormReducer';
 
 export default combineReducers({
+    // This is what generates the state using the corresponding reducer
     // The auth piece of state is produced by the AuthReducer
-    auth: AuthReducer
+    auth: AuthReducer,
+    employeeForm: EmployeeFormReducer
 });
