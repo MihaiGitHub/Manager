3bc4c5e381c976cffd5b541a71537e98151858b5
Michael S <mihai.sanfran@gmail.com>
2019-01-03 10:59:18 -0700

    Refactor and create EmployeeActions separately

---
+++ src/actions/AuthActions.js
@@ -0,0 +1,54 @@
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+import { 
+    EMAIL_CHANGED, 
+    PASSWORD_CHANGED, 
+    LOGIN_USER_SUCCESS, 
+    LOGIN_USER_FAIL,
+    LOGIN_USER
+} from './types';
+
+export const emailChanged = (text) => {
+    return {
+        type: EMAIL_CHANGED,
+        payload: text
+    };
+};
+
+export const passwordChanged = (text) => {
+    return {
+        type: PASSWORD_CHANGED,
+        payload: text
+    };
+};
+
+// Async action creator to be called with an email and password
+export const loginUser = ({ email, password }) => {
+    // Action creator returning a function; Redux thunk calls this function
+    // Manually dispatching action when the async request is complete and returning type and payload
+    return (dispatch) => {
+        // To show spinner while user is being logged in
+        dispatch({ type: LOGIN_USER });
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(user => loginUserSuccess(dispatch, user))
+        .catch(() => {
+            firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(email, password)
+                .then(user => loginUserSuccess(dispatch, user))
+                .catch(() => loginUserFail(dispatch));
+        });
+    };
+};
+
+const loginUserFail = (dispatch) => {
+    dispatch({ type: LOGIN_USER_FAIL });
+}
+
+const loginUserSuccess = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user
+    });
+    // main = key from Scene; Change scene to employeeList after login success
+    Actions.main();
+};
--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -4,6 +4,7 @@ import {
 
 // One action creator that can update any different prop that exists inside the form
 export const employeeCreate = ({ prop, value }) => {
+    console.log('in action employee create')
     return {
         type: EMPLOYEE_CREATE,
         payload: { prop, value }
