9d39470c7ebd50ab43c8a3df1766083af680befd
Michael S <mihai.sanfran@gmail.com>
2019-01-04 16:10:24 -0700

    Add event handler to call action creator on button click

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { Picker, Text, View } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardSection, InputAccessoryView, Button, Input } from './common';
 
 class EmployeeCreate extends Component {
+    onButtonPress(){
+        const { name, phone, shift } = this.props;
+
+        // have access to this action in props because it is in the connect function at the bottom
+        // If a shift is not provided than use Monday
+        this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+    }
+
     render(){
         return (
             <Card>
@@ -45,7 +53,7 @@ flexDirection: 'column' will place the CardSection children in a column (one on 
                 </CardSection>
 
                 <CardSection>
-                    <Button>Create</Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>Create</Button>
                 </CardSection>
             </Card>
         );
@@ -68,4 +76,4 @@ const mapStateToProps = (state) => {
     return { name, phone, shift };
 }
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, { employeeUpdate, employeeCreate })(EmployeeCreate);
