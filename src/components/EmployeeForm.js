d935d313723b422bf745e2aa77a16bf9222909a1
Michael S <mihai.sanfran@gmail.com>
2019-04-14 17:29:09 -0700

    Create EmployeeForm component to be used for both Employee create and Employee edit

---
+++ src/components/EmployeeForm.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { View, Text, Picker } from 'react-native';
+import { CardSection, Input } from './common';
+
+class EmployeeForm extends Component {
+    render() {
+        return (
+            <View>
+                <CardSection>
+                    <Input 
+                        label="Name" 
+                        placeholder="Jane" 
+                        value={this.props.name} 
+                        onChangeText={text => this.props.employeeUpdate({ prop: 'name', value: text })} />
+                </CardSection>
+
+                <CardSection>
+                    <Input 
+                        label="Phone" 
+                        placeholder="555-555-5555" 
+                        value={this.props.phone} 
+                        onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text })} />
+                </CardSection>
+{/* This is a custom created component and style is only a property passed down into it 
+Cardsection default style is flexDirection: 'row' (all it's children being layed out left to right)  
+flexDirection: 'column' will place the CardSection children in a column (one on top of the other)*/}
+               <CardSection style={{ flexDirection: 'column' }}>
+                    <Text style={styles.pickerTextStyle}>Shift</Text>
+
+                        <Picker style={{ width: '90%', alignSelf: 'center' }}
+                            selectedValue={this.props.shift} 
+                            onValueChange={day => this.props.employeeUpdate({ prop: 'shift', value: day })} 
+                        >
+                            <Picker.Item label="Monday" value="Monday" />
+                            <Picker.Item label="Tuesday" value="Tuesday" />
+                            <Picker.Item label="Wednesday" value="Wednesday" />
+                            <Picker.Item label="Thursday" value="Thursday" />
+                            <Picker.Item label="Friday" value="Friday" />
+                            <Picker.Item label="Saturday" value="Saturday" />
+                            <Picker.Item label="Sunday" value="Sunday" />
+                        </Picker>
+
+                </CardSection>
+            </View>
+        );
+    }
+}
+
+const styles = {
+    pickerTextStyle: {
+        fontSize: 18,
+        paddingLeft: 25
+    }
+};
+
+export default EmployeeForm;
