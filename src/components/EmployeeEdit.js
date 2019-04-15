3e5bb0e3cace92dc8b16ce6877aca68505cd24ef
Michael S <mihai.sanfran@gmail.com>
2019-04-14 19:26:53 -0700

    Initialize employee form from state

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,17 +1,32 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 // Connect component to redux to access app state and action creators
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
+// This action creator is what changes the state of the reducer with new employee data
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Button } from './common';
-import { connect } from 'net';
 
 class EmployeeEdit extends Component {
+    componentWillMount(){
+        // Loop through the employee model coming into this component
+        // Update reducer with every property on that object
+        _.each(this.props.employee, (value, prop) => {
+            this.props.employeeUpdate({ prop, value });
+        });
+    }
+
+    onButtonPress(){
+        const { name, phone, shift } = this.props;
+
+    }
+
     render(){
         return (
             <Card>
                 <EmployeeForm />
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Save Changes
                     </Button>
                 </CardSection>
@@ -20,4 +35,10 @@ class EmployeeEdit extends Component {
     }
 }
 
-export default connect()(EmployeeEdit);
+const mapStateToProps = (state) => {
+    const { name, phone, shift } = state.employeeForm;
+
+    return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
